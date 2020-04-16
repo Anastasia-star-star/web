5cd827c83db0c07a74f1b7217eb812a727164e55
Anastasia_star_star <aiarkhipova@yandex.ru>
2020-04-16 20:03:22 +0300

    My programm

---
+++ venv/app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template, request
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/index')
+def index():
+    user = "Ученик Яндекс.Лицея"
+
+    return render_template('index.html', title='Домашняя страница')
+
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1')
